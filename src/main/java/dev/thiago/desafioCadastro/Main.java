eeaf213e7e7b42a1d4ac2b999156e59fc1a453b5
Thiago <thiagoestevao3030@gmail.com>
2025-08-20 12:07:22 -0300

    Passo 1 concluído

--- src/main/java/dev/thiago/desafioCadastro/Main.java
+++ src/main/java/dev/thiago/desafioCadastro/Main.java
@@ -1,9 +1,15 @@
 package main.java.dev.thiago.desafioCadastro;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import main.java.dev.thiago.desafioCadastro.services.LerFormulario;
+
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello world!");
+
+        LerFormulario read = new LerFormulario();
+
+        read.lerFormulario();
+
     }
 }
